a6484397fe5da7dfcdeb86376758b956b873adc5
Matias Camiletti <matias.camiletti@gmail.com>
2022-12-26 12:23:19 -0300

    Update to Angular 9

--- projects/ngx-org-chart/src/public-api.ts
+++ projects/ngx-org-chart/src/public-api.ts
@@ -2,4 +2,12 @@
  * Public API Surface of ngx-org-chart
  */
 
+/**
+ * Components
+ */
+export * from './lib/ngx-org-chart/ngx-org-chart.component';
+
+/**
+ * Modules
+ */
 export * from './lib/ngx-org-chart/ngx-org-chart.module';
